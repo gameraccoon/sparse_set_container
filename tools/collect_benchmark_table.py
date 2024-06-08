cb519c602f37f00b57f9966556bdb7d596b68c58
Pavel Grebnev <ruwhynot@gmail.com>
2024-06-08 13:23:46 +0200

    Added a script to collect benchmark results into a table

---
+++ tools/collect_benchmark_table.py
@@ -0,0 +1,117 @@
+import os
+import re
+import subprocess
+
+benchmark_output = subprocess.run(['cargo', 'run', '--example', 'bench', '--release'], stdout=subprocess.PIPE)
+benchmark_output = benchmark_output.stdout.decode('utf-8')
+
+benchmark_groups = [
+    {
+        'name': 'SparseSet<String>',
+        'suffix': '_sparse_set'
+    },
+    {
+        'name': 'Vec<String>',
+        'suffix': '_vec'
+    },
+    {
+        'name': 'HashMap<i32, String>',
+        'suffix': '_hash_map'
+    }
+]
+
+benchmarks = [
+    {
+        'name': 'Create empty',
+        'benchmark_name': 'create_empty',
+    },
+    {
+        'name': 'Create with capacity',
+        'benchmark_name': 'create_with_capacity',
+    },
+    {
+        'name': 'Push 100 elements',
+        'benchmark_name': 'push_hundred_elements',
+    },
+    {
+        'name': 'With capacity push 100',
+        'benchmark_name': 'create_with_capacity_and_push_hundred_elements',
+    },
+    {
+        'name': 'Lookup 100 elements',
+        'benchmark_name': 'get_hundred_elements',
+    },
+    {
+        'name': 'Iterate over 100 elements',
+        'benchmark_name': 'iterate_over_hundred_elements',
+    },
+    {
+        'name': 'Clone with 100 elements',
+        'benchmark_name': 'clone_with_hundred_elements',
+    },
+    {
+        'name': 'Clone 100 and remove 10',
+        'benchmark_name': 'clone_and_remove_ten_out_of_hundred_elements',
+    },
+    {
+        'name': 'Clone 100 and swap_remove 10',
+        'benchmark_name': 'clone_and_swap_remove_ten_out_of_hundred_elements',
+    },
+]
+
+def parse_benchmark_line(line):
+    parts = line.split()
+    name = parts[1]
+    time = parts[4]
+    error = parts[7].strip(')')
+    return name, time, error
+
+
+def parse_benchmark_output(output):
+    lines = output.split('\n')
+    # ignore lines that don't end with ')'
+    lines = [line for line in lines if line.endswith(')')]
+    # get dict of benchmark results
+    benchmarks = {}
+    for line in lines:
+        name, time, error = parse_benchmark_line(line)
+        benchmarks[name] = (time, error)
+    return benchmarks
+
+
+print("Started collecting benchmarks...")
+
+benchmark_results = parse_benchmark_output(benchmark_output)
+
+print("Benchmarks collected")
+
+# make a table for printing
+table = []
+# header
+table.append(['Benchmark'] + ["`" + group['name'] + "`" for group in benchmark_groups])
+for benchmark in benchmarks:
+    row = [benchmark['name']]
+    bench_name = benchmark['benchmark_name']
+    for group in benchmark_groups:
+        suffix = group['suffix']
+        key = bench_name + suffix
+        if key in benchmark_results:
+            time, error = benchmark_results[key]
+            row.append(f'{time} ns ±{error}')
+        else:
+            row.append('N/A')
+
+    table.append(row)
+
+# print a markdown table
+table_str = ''
+for row in table:
+    table_str += '| ' + ' | '.join(row) + ' |\n'
+    if row == table[0]:
+        table_str += '| ' + ' | '.join(['---' for _ in row]) + '|\n'
+
+bench_file_name = 'bench_table.md'
+with open(bench_file_name, 'w') as f:
+    f.write(table_str)
+
+print(f"New data has been written to {bench_file_name}")
--- tools/update_readme.py
+++ tools/update_readme.py
@@ -38,11 +38,21 @@ def replace_examples(readme):
         with open(f'examples/{block}.rs', 'r') as file:
             code = file.read()
 
+        code = code.strip()
+
         replacement = f'\n```rust\n{code}\n```\n'
         return readme[:start_index + len(start)] + replacement + readme[end_index:]
 
 
 def update_benchmark_results(readme):
+    benchmark_file = 'bench_table.md'
+
+    if not os.path.exists(benchmark_file):
+        print(f'Benchmark file {benchmark_file} not found, skipping benchmark results update')
+        return readme
+
+    print("Found benchmark results")
+
     start = '<!--benchmark table start-->'
     end = '<!--benchmark table end-->'
 
@@ -53,14 +63,12 @@ def update_benchmark_results(readme):
         print('Benchmark table not found')
         return readme
 
-    if not os.path.exists('results.txt'):
-        print('results.txt not found, skipping benchmark table update')
-        return readme
-
-    with open('results.txt', 'r') as file:
+    with open(benchmark_file, 'r') as file:
         results = file.read()
 
-    return readme[:start_index + len(start)] + results + readme[end_index:]
+    results = results.strip()
+
+    return readme[:start_index + len(start)] + "\n" +results + "\n" + readme[end_index:]
 
 version = get_version()
 print(f'Latest version: {version}')
