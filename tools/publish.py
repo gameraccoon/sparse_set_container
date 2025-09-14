158b4b9ca0dbe0375673e32a50b1e38ab4614baa
Pavel Grebnev <ruwhynot@gmail.com>
2025-09-14 11:00:48 +0200

    Added running tests, checking compilation of examples and checking for the existence of the tag to the publish script

--- tools/publish.py
+++ tools/publish.py
@@ -8,7 +8,7 @@ os.system("python tools/update_readme.py")
 # check that git doesn't have any changes
 status = os.popen("git status --porcelain").read()
 if status:
-    print("Git has changes, commit them first")
+    print("\nGit has changes, commit them first")
     exit(1)
 
 # get the version after @ or # from the result of `cargo pkgid`
@@ -19,30 +19,54 @@ if len(pkgid_output_split) == 1:
 version = pkgid_output_split[1].strip()
 print(f"Current version is '{version}'")
 
+# check that git doesn't have the tag locally
+if subprocess.run(["git", "tag", "-l", f"v{version}"], capture_output=True).stdout.decode("utf-8").strip() != "":
+    print(f"\nTag v{version} already exists in the git repo locally, exiting")
+    exit(1)
+
+# check that git doesn't have the tag remotely
+if subprocess.run(["git", "ls-remote", "--tags", "origin", f"refs/tags/v{version}"], capture_output=True).stdout.decode("utf-8").strip() != "":
+    print(f"\nTag v{version} already exists in the git repo on the remote, exiting")
+    exit(1)
+
+# check that examples can be compiled
+if subprocess.run(["cargo", "build", "--examples", "--quiet"], capture_output=True).returncode != 0:
+    os.system("cargo build --examples --quiet")
+    print("\nExamples failed to compile, exiting")
+    exit(1)
+print("Readme examples compiled successfully")
+
+# check that tests are passing (suppress test output)
+if subprocess.run(["cargo", "test", "--quiet"], capture_output=True).returncode != 0:
+    os.system("cargo test --quiet --no-fail-fast")
+    print("\nTests failed, exiting")
+    exit(1)
+print("Tests passed")
+
 push = "--push" in sys.argv
 if push:
     print("Pushing all changes to git")
     exit_code = os.system("git push")
     if exit_code != 0:
-        print("Failed to push changes, exiting")
+        print("\nFailed to push changes, exiting")
         exit(1)
 
     print("Creating a new tag in git")
     exit_code = os.system(f"git tag v{version}")
     if exit_code != 0:
-        print("Failed to create a tag, exiting")
+        print("\nFailed to create a tag, exiting")
         exit(1)
 
     print("Pushing the tag to git")
     exit_code = os.system("git push --tags")
     if exit_code != 0:
-        print("Failed to push the tag, exiting")
+        print("\nFailed to push the tag, exiting")
         exit(1)
 
     print("Publishing to crates.io")
     exit_code = os.system("cargo publish")
     if exit_code != 0:
-        print("Failed to publish, exiting")
+        print("\nFailed to publish, exiting")
         exit(1)
     else:
         print("Published successfully")
@@ -52,7 +76,7 @@ else:
     exit_code = os.system("cargo publish --dry-run")
 
     if exit_code != 0:
-        print("Failed to do dry-run publish, exiting")
+        print("\nFailed to do dry-run publish, exiting")
         exit(1)
     else:
         print("Dry-run succeeded, use --push to publish to crates.io")
